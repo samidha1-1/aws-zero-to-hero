3cb076e4237656fe099bd05f029cf2e408aef6ac
samidha <samidhawani7@gmail.com>
2025-11-02 18:39:22 +0530

    commiteed

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,12 +1,9 @@
-#!/bin/bash
-set -#!/bin/bash
+
 set -e
 
-echo "Pulling latest Docker image..."
-sudo docker pull samidha7/simple-python-flask-app
-
-echo "Running new container..."
-sudo docker run -d -p 5000:5000 samidha7/simple-python-flask-app
 
 
+docker pull samidha7/simple-python-flask-app
+
+docker run -d -p 5000:5000 samidha7/simple-python-flask-app
 
--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,19 +1,7 @@
 #!/bin/bash
 set -e
 
-echo "Stopping and removing existing container (if running)..."
-
-# Stop any running container using the same image
-container_id=$(sudo docker ps -q --filter ancestor=samidha7/simple-python-flask-app)
-
-if [ -n "$container_id" ]; then
-    echo "Stopping container ID: $container_id"
-    sudo docker stop $container_id
-    echo "Removing container ID: $container_id"
-    sudo docker rm $container_id
-else
-    echo "No running container found for image samidha7/simple-python-flask-app"
-fi
+echo "hii"
 
 
 
