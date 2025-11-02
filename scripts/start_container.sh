a57eb56aecf4a9805fcca9e6dbceacab788ae1bd
samidha <samidhawani7@gmail.com>
2025-11-02 18:55:35 +0530

    cmmiteed

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,9 +1,13 @@
 
+#!/bin/bash
 set -e
 
+echo "Starting new container..."
+cd /home/ubuntu/simple-python-flask-app
 
+sudo docker build -t simple-python-flask-app .
+sudo docker run -d -p 5000:5000 simple-python-flask-app
 
-docker pull samidha7/simple-python-flask-app
+echo "Flask container started successfully!"
 
-docker run -d -p 5000:5000 samidha7/simple-python-flask-app
 
