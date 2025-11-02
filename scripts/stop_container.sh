3bc16d214582321c9846baa85617f975ffef7517
samidha <samidhawani7@gmail.com>
2025-11-02 17:01:47 +0530

    updated

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 set -e
 
-echo "Stopping and removing existing container (if running)..."
-
-# Stop the running container (ignore errors if it's not running)
-docker stop simple-python-flask-app || true
-
-# Remove the container (ignore errors if it doesn't exist)
-docker rm simple-python-flask-app || true
-
-echo "Old container stopped and removed successfully."
+# Stop the running container (if any)
+echo "Hi"
