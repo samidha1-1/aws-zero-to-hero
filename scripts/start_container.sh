f599a61d93a0847ea7d5ddef4d18da313f941e51
samidha <samidhawani7@gmail.com>
2025-11-02 19:00:48 +0530

    updated

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,12 +2,7 @@
 #!/bin/bash
 set -e
 
-echo "Starting new container..."
-cd /home/ubuntu/simple-python-flask-app
+docker pull samidha7/simple-python-flask-app:latest
 
-sudo docker build -t simple-python-flask-app .
-sudo docker run -d -p 5000:5000 simple-python-flask-app
-
-echo "Flask container started successfully!"
-
+docker run -d -p 5000:5000  samidha7/simple-python-flask-app:latest
 
--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,18 +1,7 @@
 #!/bin/bash
 set -e
 
-echo "Stopping running container (if any)..."
-
-# Find running container based on image name
-container_id=$(sudo docker ps -q --filter "ancestor=samidha7/simple-python-flask-app")
-
-if [ -n "$container_id" ]; then
-    echo "Stopping container: $container_id"
-    sudo docker stop $container_id
-    sudo docker rm $container_id
-else
-    echo "No running container found."
-fi
+echo "hi"
 
 
 
