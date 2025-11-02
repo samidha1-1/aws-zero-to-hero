edc1d1b0506a14098a6b01f7cd3629cea0e93d58
samidha <samidhawani7@gmail.com>
2025-11-02 16:49:10 +0530

    changes updates

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull samidha7/simple-python-flask-app:latest
+docker pull samidha7/simple-python-flask-app
 
 # Run the Docker image as a container
-docker run -it -p 5000:5000 samidha7/simple-python-flask-app:latest
+docker run -it -p 5000:5000 samidha7/simple-python-flask-app
